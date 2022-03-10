f258993b57c0f7db0de12e284d980dcaa78e8c12
The Trading Guy <98614666+xtekky@users.noreply.github.com>
2022-03-10 01:13:03 +0100

    Update login.py

--- login.py
+++ login.py
@@ -6,14 +6,15 @@ from selenium.webdriver.common.by import By
 mail_address = 'email\n' # replace email
 password = 'passsword\n' # replace password
 #  ---------- EDIT ---------- 
-
+wait = WebDriverWait(driver, 20)
 driver = uc.Chrome(use_subprocess=True)
 url = 'https://accounts.google.com/ServiceLogin?service=accountsettings&continue=https://myaccount.google.com%3Futm_source%3Daccount-marketing-page%26utm_medium%3Dgo-to-account-button'
 driver.get(url)
 
-driver.find_element(By.NAME, 'identifier').send_keys(mail_address)
+
+wait.until(EC.visibility_of_element_located(By.NAME, 'identifier').send_keys(mail_address)
 sleep(2)
-driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
+wait.until(EC.visibility_of_element_located(By.NAME, 'password').send_keys(password)
 print("You're in!! enjoy")
 
 
