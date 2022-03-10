9f5abb7d9d6ab1f0f9844c05eb22c466cbf5bbb0
The Trading Guy <98614666+xtekky@users.noreply.github.com>
2022-03-10 01:25:41 +0100

    Update login.py

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 #  ---------- EDIT ----------
-mail_address = 'email\n' # replace email
+email = 'email\n' # replace email
 password = 'password\n' # replace password
 #  ---------- EDIT ----------
 
@@ -14,7 +14,7 @@ url = 'https://accounts.google.com/ServiceLogin?service=accountsettings&continue
 driver.get(url)
 
 
-wait.until(EC.visibility_of_element_located((By.NAME, 'identifier'))).send_keys(mail_address)
+wait.until(EC.visibility_of_element_located((By.NAME, 'identifier'))).send_keys(email)
 wait.until(EC.visibility_of_element_located((By.NAME, 'password'))).send_keys(password)
 print("You're in!! enjoy")
 
