2be3297941063d9de6e1550634573ed70e551baa
Tekky <98614666+xtekky@users.noreply.github.com>
2022-06-26 04:42:38 +0200

    Update login.py

--- login.py
+++ login.py
@@ -10,8 +10,8 @@ password = 'password\n' # replace password
 
 driver = uc.Chrome(use_subprocess=True)
 wait = WebDriverWait(driver, 20)
-url = 'https://accounts.google.com/ServiceLogin?service=accountsettings&continue=https://myaccount.google.com%3Futm_source%3Daccount-marketing-page%26utm_medium%3Dgo-to-account-button'
-driver.get(url)
+
+driver.get('https://accounts.google.com/ServiceLogin')
 
 
 wait.until(EC.visibility_of_element_located((By.NAME, 'identifier'))).send_keys(email)
