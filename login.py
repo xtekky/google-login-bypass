307584b99ef6eecc0aedc057bf6d3a9f9fe98f42
t.me/xtekky <98614666+xtekky@users.noreply.github.com>
2023-02-12 15:38:02 +0100

    update

--- login.py
+++ login.py
@@ -1,29 +1,32 @@
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
+from undetected_chromedriver       import Chrome
+from time                          import sleep
+from selenium.webdriver.common.by  import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support    import expected_conditions as EC
 
-class Main:
-  def __init_(self) -> None:
-    self.url    = 'https://accounts.google.com/ServiceLogin'
-    self.driver = driver = uc.Chrome(use_subprocess=True)
-    self.time   = 10
+class Google:
+    def __init__(self) -> None:
+        self.url    = 'https://accounts.google.com/ServiceLogin'
+        self.driver = Chrome(use_subprocess=True); self.driver.get(self.url)
+        self.time   = 10
     
-  def login(self, email, password):
-    WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.NAME, 'identifier'))).send_keys(f'{email}\n')
-    WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.NAME, 'password'))).send_keys(f'{password}\n')
-                                                                                
-    self.code()
-                                                                                  
-  def code(self):
+    def login(self, email, password):
+        sleep(2)
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.NAME, 'identifier'))).send_keys(f'{email}\n')
+        sleep(2)
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.NAME, 'Passwd'))).send_keys(f'{password}\n')
+
+        self.code()
+
+    def code(self):
     # [ ---------- paste your code here ---------- ]
-    time.sleep(self.time)                                                                                  
-                                                                                  
+        sleep(self.time)                                                                                  
+
 if __name__ == "__main__":
-  #  ---------- EDIT ----------
-  email = 'email' # replace email
-  password = 'password' # replace password
-  #  ---------- EDIT ----------                                                                                                                                                         
- 
-  driver = Main()
-  driver.login(email, password)                                                                                
+    #  ---------- EDIT ----------
+    email = 'email' # replace email
+    password = 'password' # replace password
+    #  ---------- EDIT ----------                                                                                                                                                         
+    
+    google = Google()
+    google.login(email, password)
